82ccf68659995c4ef4f54ddcd392f85361e1f5e9
Rsantct <rsantct@gmail.com>
2018-11-10 11:07:19 +0100

    Update functions.php

--- www/php/functions.php
+++ www/php/functions.php
@@ -1,36 +1,51 @@
 <?php
 
     // Para depurar https://www.ibm.com/developerworks/library/os-debug/index.html
+    // En nuestro caso también podemos hacer aquí echo $miVariable, que será recibida por
+    // la función JS que haya hecho la http request en el cliente y printarlo allí en 
+    // la consola del navegador con console.log(respuesta).
 
     // Este código PHP reside localmente en el server y no es visible desde el navegador.
-    // Este PHP será cargado por Apache (si tiene enabled el php_mod),
-    // entonces se ESCUCHARÁN HTTP_REQUEST como por ejemplo:
+    // Este PHP sera cargado por Apache (si tiene enabled el php_mod),
+    // entonces se ESCUCHARAN HTTP_REQUEST como por ejemplo:
     //     "GET", "php/functions.php?command=level -15"
-    // que son generadas por el js del cliente.
-    // Las RESPUESTAS a dichas request se devolverán mediante el 'echo' al final de este código.
+    // que son generadas por el JS del cliente.
+    // Las RESPUESTAS se devolverán mediante el 'echo' al final de este código.
 
-    // Definimos 'command' como el argumento que va a recibir este script PHP server side
-    // mediaente las HttpRequest originadas por el código javascript cliente, que es 
-    // cargado en el navegador (forma parte del http de la peich)
-    $command = $_REQUEST["command"];
+    // Este script debe conocer el nombre del altavoz running para acceder al path
+    // adecuado a la hora de proporcionar un archivo speaker.yml al cliente
+    // (he preferido que el código js del cliente no muestre paths del servidor)
+    function get_loudspeaker() {
+        $tmp = "";
+        $cfile = fopen("/home/predic/config/config.yml", "r") 
+                  or die("Unable to open file!");
+        while( !feof($cfile) ) {
+            $linea = fgets($cfile);
+            $found = strpos( $linea, "loudspeaker");
+            if ( $found !== false ) {
+                $tmp = str_replace( "\n", "", $linea);
+                $tmp = str_replace( "loudspeaker:", "", $tmp);
+                $tmp = trim($tmp);
+            }
+        }
+        fclose($cfile);
+        return $tmp;
+    }
 
-    // Inicializamos la variable para devolver resultados ante una request
-    $result = null;
-
-    // Función que dialoga con el server TCP/IP de PRE.DI.C
+    // Dialoga con el server TCP/IP de PRE.DI.C
     function predic_socket ($cmd) {
         $service_port = 9999;
         $address = "127.0.0.1";
-        /* Crear un socket TCP/IP. */
+        /* Crear un socket TCP/IP */
         $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
         if ($socket === false) {
-            echo "socket_create() falló: razón: " . socket_strerror(socket_last_error()) . "\n";
+            echo "socket_create() fallo: razon: " . socket_strerror(socket_last_error()) . "\n";
         }
         $result = socket_connect($socket, $address, $service_port);
         if ($result === false) {
-            echo "socket_connect() falló.\nRazón: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
+            echo "socket_connect() fallo.\nRazon: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
         }
-        // Envia el comando y recibe la respuesta
+        // Envía el comando y recibe la respuesta
         socket_write($socket, $cmd, strlen($cmd));
         $out = socket_read($socket, 4096);
         // Le dice al server que cierre la conexión en su lado
@@ -41,21 +56,37 @@
         return $out;
     }
 
-    // Lee el archivo inputs.yml tal cual
-    $inputs_file = "/home/predic/config/inputs.yml";
-    function predic_inputs() {
-        return readfile("/home/predic/config/inputs.yml"); // no funciona readfile($inputs_file); ¿!?
+    ///////////////////   MAIN: ///////////////////////
+    // Procesa el COMMAND recibido en la HTTPREQUEST
+    // y devuelve resultados mediante 'echo xxxxx'
+    ///////////////////////////////////////////////////
+    
+    // http://php.net/manual/en/reserved.variables.request.php
+    // PHP server side recibe arrays asociativas, o sea diccionarios, mediante los métodos
+    // GET o PUT de las HTTPREQUEST originadas por un código del cliente, en nuestro caso
+    // es el JS cargado en el navegador que forma parte de la peich.
+    // La array es lo que el cliente pone detrás del interrogante, por ejemplo:
+    //          "GET", "php/functions.php?command=level -15"
+    // Como es un array, debemos seleccionar la clave que nos interesa:
+    $command = $_REQUEST["command"];
+
+    // Comandos especiales:
+    if ( $command == "read_inputs_file" ) {
+        // OjO readfile hace el solito un echo del contenido del archivo, a saco.
+        readfile("/home/predic/config/inputs.yml");
+    }
+    elseif ( $command == "read_config_file" ) {
+        readfile("/home/predic/config/config.yml");
+    }
+    elseif ( $command == "read_speaker_file" ) {
+        $fpath = "/home/predic/loudspeakers/".get_loudspeaker()."/speaker.yml";
+        readfile($fpath);
     }
 
-    if ( $command == "read_inputs_file" ) {
-        $result = predic_inputs();
-    }
+    // Comandos estandar para PRE.DI.C:
     else {
-        $result = predic_socket($command);
+        echo predic_socket($command);
     }
         
-    // PHP devuelve resultados mediante 'echo xxxxx'
-    echo $result;
-    
 ?>
 
