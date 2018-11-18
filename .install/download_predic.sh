5d2eac3b4a50e72a155d8aaa22e4bd13847e7334
Rsantct <rsantct@gmail.com>
2018-11-18 23:11:10 +0100

    Create download_predic.sh

---
+++ .install/download_predic.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z $1 ] ; then
+    echo usage:
+    echo "    download_predic.sh  master | another_branch"
+    echo
+    exit 0
+fi
+branch=$1
+
+# Prepare temp directory
+mkdir /home/predic/tmp > /dev/null 2>&1
+cd /home/firtro/tmp
+
+# Removes any existent master.zip or predic directory for this branch:
+rm $branch.zip
+rm -r pre.di.c-$branch
+
+# Downloads the zip
+wget https://github.com/rsantct/pre.di.c/archive/$branch.zip
+
+# Unzip
+unzip $branch.zip
+rm $branch.zip
+
+# Drops install scripts into tmp/ to be accesible
+cp -f pre.di.c-$branch/.install/*sh .
+
+# And back to home
+cd
