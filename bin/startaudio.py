9c6914bbbf2e2c61cf9947027d61519c23715856
Rsantct <rsantct@gmail.com>
2018-11-25 22:46:12 +0100

    Update startaudio.py

--- bin/startaudio.py
+++ bin/startaudio.py
@@ -33,7 +33,7 @@
 import sys
 import os
 import time
-from subprocess import Popen, check_output
+from subprocess import Popen, check_output, run
 
 import stopaudio
 import predic as pd
@@ -60,11 +60,26 @@ def limit_level(level_on_startup, max_level_on_startup):
         level = gc.state['level']
     pd.client_socket('level ' + str(level))
 
-
 def init_jack():
     """loads jack server"""
 
+    # Some players uses ALSA as audio back end, let's reroute then to JACK
+    def update_asoundrc():
+        """ updates the jack plugin in .asoundrc
+            to point to brutefir or ecasound
+        """
+        if gc.config['load_ecasound']:
+            cmd_L = '/:in.L/c\\        0 ecasound:in_1'
+            cmd_R = '/:in.R/c\\        1 ecasound:in_2'
+        else:
+            cmd_L = '/:in_1/c\\        0 brutefir:in.L'
+            cmd_R = '/:in_2/c\\        1 brutefir:in.R'
+        run( ['sed', '-i', '-e', cmd_L, bp.main_folder + '.asoundrc'] )
+        run( ['sed', '-i', '-e', cmd_R, bp.main_folder + '.asoundrc'] )
+
     print('\n(startaudio) starting jack\n')
+
+    # Prepare the jackd command line
     jack_cmd_list = ([gc.config['jack_path']]
                         + gc.config['jack_options'].split()
                         + ['-r'] + [str(gc.speaker['fs'])])
@@ -73,10 +88,13 @@ def init_jack():
     elif not 'dummy' in gc.config['jack_options']:
         print('\n(startaudio) error starting jack: unknown backend')
         sys.exit(-1)
+
+    # Some systems runs Pulseaudio, then it is needed to use pasuspender to lauch jackd
     if 'pulseaudio' in check_output("pgrep -fl pulseaudio", shell=True).decode():
         jack = Popen(['pasuspender', '--'] + jack_cmd_list)
     else:
         jack = Popen(jack_cmd_list)
+
     # waiting for jackd:
     if pd.wait4result('jack_lsp', 'system', tmax=10):
         print('\n(startaudio) jack started :-)')
@@ -84,6 +102,7 @@ def init_jack():
         print('\n(startaudio) error starting jack')
         sys.exit()
 
+    update_asoundrc()
 
 def init_brutefir():
     """loads brutefir"""
