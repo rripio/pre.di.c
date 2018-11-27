7ce49114997817ba08a26dce9df00726e019dcb7
Rsantct <rsantct@gmail.com>
2018-11-27 12:31:28 +0100

    Update players.py

--- bin_custom/players.py
+++ bin_custom/players.py
@@ -18,45 +18,49 @@ def get_inputs():
     f.close()
     return yaml.load(tmp)
 
-def find_jack_ports(pattern):
-    jc = jack.Client('tmp_players')
-    result = False
-    if jc.get_ports(name_pattern=pattern):
-        result = True
-    jc.close()
-    return result
-
-def get_librespot_info():
-
-    try:
-        # Returns the current track played by librespot when redirected to ~/tmp/.librespotEvents
-        tmp = sp.check_output( 'tail -n1 /home/predic/tmp/.librespotEvents'.split() )
-        tmp = tmp.decode().split('"')[-2]
-        # JSON for JavaScript on control web page, NOTICE json requires double quotes:
-        return '{ "player":"Spotify", "artist":"", "album":"", "title":"' + tmp + '" }'
-
-    except:
-        return '{}'
-
 def get_mpd_info(mpd_host='localhost', mpd_port=6600, mpd_passwd=None):
     """ gets playing info from mpd
     """
-    # WORK IN PROGRESS
-
     client = mpd.MPDClient()
     client.connect(mpd_host, mpd_port)
     if mpd_passwd is not None:
         client.password(mpd_passwd)
-    print( client.currentsong() )
+    artist = client.currentsong()['artist']
+    album  = client.currentsong()['album']
+    title  = client.currentsong()['title']
     client.close()
 
-    return '{}'
+    return '{ "player":"MPD", "artist":"' + artist + '", "album":"' + album + '", "title":"' + title + '" }'
+
+def get_librespot_info():
+    # Unfortunately librespot only prints out the title metadata, nor artist neither album.
+    # More info can be retrieved from the spotify web, but it is necessary to autenticathe.
+
+    try:
+        # Returns the current track played by librespot when redirected to ~/tmp/.librespotEvents
+        tmp = sp.check_output( 'tail -n1 /home/predic/tmp/.librespotEvents'.split() )
+        artist = ''
+        album  = ''
+        title  = tmp.decode().split('"')[-2]
+        # JSON for JavaScript on control web page, NOTICE json requires double quotes:
+        return '{ "player":"MPD", "artist":"' + artist + '", "album":"' + album + '", "title":"' + title + '" }'
+
+    except:
+        return '{}'
 
 def get_current_player():
     # WORK IN PROGRESS
     # Gets the current player based on the current input name.
     # Neverless, some players does not have jack backend, then using
     # the alsa to jack plugin and none input is selected on pre.di.c
+
+    def find_jack_ports(pattern):
+        jc = jack.Client('tmp_players')
+        result = False
+        if jc.get_ports(name_pattern=pattern):
+            result = True
+        jc.close()
+        return result
 
     curr_input =  get_state()['input']
 
