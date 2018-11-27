9897559c095122beeed4a5e574acd8f7fbde8f6f
Rsantct <rsantct@gmail.com>
2018-11-27 10:19:26 +0100

    Update players.py

--- bin_custom/players.py
+++ bin_custom/players.py
@@ -1,12 +1,63 @@
 #!/usr/bin/env python3
-
+""" A custom made script to retrieve track info from the current player
+"""
 import subprocess as sp
+import yaml
+import jack
+
+def get_state():
+    f = open('/home/predic/config/state.yml', 'r')
+    tmp = f.read()
+    f.close()
+    return yaml.load(tmp)
+
+def get_inputs():
+    f = open('/home/predic/config/inputs.yml', 'r')
+    tmp = f.read()
+    f.close()
+    return yaml.load(tmp)
+
+def find_jack_ports(pattern):
+    jc = jack.Client('tmp_players')
+    result = False
+    if jc.get_ports(name_pattern=pattern):
+        result = True
+    jc.close()
+    return result
+
+def get_librespot_info():
+
+    try:
+        # Returns the current track played by librespot when redirected to ~/tmp/.librespotEvents
+        tmp = sp.check_output( 'tail -n1 /home/predic/tmp/.librespotEvents'.split() )
+        tmp = tmp.decode().split('"')[-2]
+        # JSON for JavaScript on control web page, NOTICE json requires double quotes:
+        return '{ "player":"Spotify", "artist":"", "album":"", "track":"' + tmp + '" }'
+
+    except:
+        return '{}'
+
+def get_mpd_info():
+    # WORK IN PROGRESS
+    return '{}'
 
 def get_current_player():
-    # WORK IN PROGRESS    
-    # It is planned to check for the current player ...
-    # 
-    return 'spotify'
+    # WORK IN PROGRESS
+    # Gets the current player based on the current input name.
+    # Neverless, some players does not have jack backend, then using
+    # the alsa to jack plugin and none input is selected on pre.di.c
+
+    curr_input =  get_state()['input']
+
+    if curr_input == 'none':
+        # if find_jack_ports('alsa'): # but alsa ports disappears when changing track :-/
+        return 'spotify'
+
+    elif curr_input == 'mpd':
+        return 'mpd'
+
+    else:
+        return 'none'
 
 def get_current_playing():
     # Retrieve a dictionary with the current player info
@@ -17,24 +68,8 @@ def get_current_playing():
     if player == 'spotify':
         return get_librespot_info()
 
-    elif player() == 'mpd':
+    elif player == 'mpd':
         return get_mpd_info()
 
     else:
         return '{}'
-        
-def get_librespot_info():
-
-    try:
-        # Returns the current track played by librespot when redirected to ~/tmp/.librespotEvents
-        tmp = sp.check_output( 'tail -n1 /home/predic/tmp/.librespotEvents'.split() )
-        tmp = tmp.decode().split('"')[-2]
-        # JSON for JavaScript on www, NOTICE required double quotes:
-        return '{ "player":"Spotify", "artist":"", "album":"", "track":"' + tmp + '" }'
-
-    except:
-        return '{}'
-
-def get_mpd_info():
-    # WORK IN PROGRESS
-    return '{}'
