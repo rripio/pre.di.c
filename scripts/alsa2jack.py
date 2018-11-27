942d2edb7aa2a5569bf3ccd5d4a7be02f750563e
Rsantct <rsantct@gmail.com>
2018-11-27 19:50:35 +0100

    Create alsa2jack.py

---
+++ scripts/alsa2jack.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+""" sets up alsa_loop ports in jack to be used from ALSA backended players
+
+    use:    alsa2jack.py   start | stop
+"""
+
+import sys
+from subprocess import run
+import jack_loop
+
+def start():
+    # jack_loop module will keep the loop alive
+    jack_loop.jack_loop('alsa_loop')
+
+def stop():
+    run( 'pkill -f alsa2jack.py'.split() )
+    sys.exit()
+
+if sys.argv[1:]:
+    try:
+        option = {
+            'start' : start,
+            'stop'  : stop
+            }[ sys.argv[1] ]()
+    except:
+        print('(alsa2jack) bad option')
+else:
+    print(__doc__)
