7ceb01cbc4538e14845281c42fce5ac890442d29
Rsantct <rsantct@gmail.com>
2018-12-04 10:00:46 +0100

    Update functions.php

--- www/php/functions.php
+++ www/php/functions.php
@@ -1,8 +1,33 @@
 <?php
 
+    /*
+    Copyright (c) 2018 Rafael Sánchez
+    This file is part of pre.di.c
+    pre.di.c, a preamp and digital crossover
+    Copyright (C) 2018 Roberto Ripio
+
+    pre.di.c is based on FIRtro https://github.com/AudioHumLab/FIRtro
+    Copyright (c) 2006-2011 Roberto Ripio
+    Copyright (c) 2011-2016 Alberto Miguélez
+    Copyright (c) 2016-2018 Rafael Sánchez
+
+    pre.di.c is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    pre.di.c is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pre.di.c.  If not, see https://www.gnu.org/licenses/.
+    */
+
     /*  Hidden server side php code.
-     *  PHP will response to the client via the standard php output (echo xxx, readfile(xx), etcetera)
-     */
+        PHP will response to the client via the standard php output (echo xxx, readfile(xx), etcetera)
+    */
 
     // Retrieves the running loudspeaker on pre.di.c
     function get_loudspeaker() {
