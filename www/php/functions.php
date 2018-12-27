d478fbf9f771a97b7f041cf940bff50a9cb28ea8
Rsantct <rsantct@gmail.com>
2018-12-27 19:01:08 +0100

    Update functions.php

--- www/php/functions.php
+++ www/php/functions.php
@@ -21,11 +21,16 @@
     along with pre.di.c.  If not, see https://www.gnu.org/licenses/.
     */
 
-    /*  Hidden server side php code.
-        PHP will response to the client via the standard php output (echo xxx, readfile(xx), etcetera)
+    /*  This is the hidden server side php code.
+        PHP will response to the client via the standard php output, for instance:
+            echo $some_varible;
+            echo "some_string";
+            readfile("some_file_path");
     */
 
-    // Retrieves configured items from pre.di.c 'config.yml' file
+    /////////////////////////////////////////////////////////////////////
+    // Retrieves simple configured items from pre.di.c 'config.yml' file
+    /////////////////////////////////////////////////////////////////////
     function get_config($item) {
         $tmp = "";
         $cfile = fopen("/home/predic/config/config.yml", "r")
@@ -36,6 +41,7 @@
             if ( $found !== false ) {
                 $tmp = str_replace( "\n", "", $linea);
                 $tmp = str_replace( $item, "", $tmp);
+                $tmp = str_replace( ":", "", $tmp);
                 $tmp = trim($tmp);
             }
         }
@@ -45,11 +51,16 @@
 
     //////////////////////////////////////////////////////
     // Communicates to the pre.di.c TCP/IP servers.
+    // Notice: server address and port are specified
+    //         into 'config/config.yml' for each service,
+    //         for instance 'control', 'players' or 'aux'.
     //////////////////////////////////////////////////////
     function predic_socket ($service, $cmd) {
+
         $address = get_config( $service."_address" );
-        $port    = get_config( $service."_port" );
-        /* Creates a TCP socket*/
+        $port    = intval( get_config( $service."_port" ) );
+        
+        // Creates a TCP socket
         $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
         if ($socket === false) {
             echo "socket_create() failed: " . socket_strerror(socket_last_error()) . "\n";
@@ -58,13 +69,14 @@
         if ($result === false) {
             echo "socket_connect() failed: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
         }
-        // Sends and receive
+        // Sends and receive:
         socket_write($socket, $cmd, strlen($cmd));
         $out = socket_read($socket, 4096);
         // Tells the server to close the connection from its end:
         socket_write($socket, "quit", strlen("quit"));
+        // Empties the receiving buffer:
         socket_read($socket, 4096);
-        // Ends this end socket
+        // And close this end socket:
         socket_close($socket);
         return $out;
     }
@@ -73,36 +85,33 @@
     // listen to http request then returns results via standard output
     ///////////////////////////////////////////////////////////////////
 
-    /* http://php.net/manual/en/reserved.variables.request.php
-    * PHP server side receives associative arrays, i.e. dictionaries, through by the 
-    * GET o PUT methods from the client side HTTPREQUEST (usually javascript).
-    * The array is what appears after 'php/functions.php?.......', example:
-    *          "GET", "php/functions.php?command=level -15"
-    * Here the key 'command' has the value 'level -15'
+    /*  http://php.net/manual/en/reserved.variables.request.php
+        PHP server side receives associative arrays, i.e. dictionaries, through by the 
+        GET o PUT methods from the client side HTTPREQUEST (usually javascript).
+        The array is what appears after 'php/functions.php?.......', example:
+                "GET", "php/functions.php?command=level -15"
+        Here the key 'command' has the value 'level -15'
+        So, lets read the key 'command':
     */
-
-    // lets read the key 'command'
     $command = $_REQUEST["command"];
 
-    //// SPECIAL commands:
-
-    // Reading config FILES
+    // READING CONFIG FILES
+    // Notice: readfile() does an 'echo', so it returns the contents to the standard php output
     if ( $command == "read_inputs_file" ) {
-        // notice: readfile() does an 'echo', i.e. it returns the contents to the standard php output
         readfile("/home/predic/config/inputs.yml");
     }
     elseif ( $command == "read_config_file" ) {
         readfile("/home/predic/config/config.yml");
     }
     elseif ( $command == "read_speaker_file" ) {
-        $fpath = "/home/predic/loudspeakers/".get_loudspeaker()."/speaker.yml";
+        $fpath = "/home/predic/loudspeakers/".get_config("loudspeaker")."/speaker.yml";
         readfile($fpath);
     }
 
-    // AMPLIFIER switching are handled by the 'aux' server
+    // AMPLIFIER switching commands are handled by the 'aux' server
+    // Notice: It is expected that the remote script will store the amplifier state
+    //         into the file '~/.ampli' so that the web can update it.
     elseif ( $command == "amplion" ) {
-        // The remote script will store the amplifier state into
-        // ~/.ampli so that the web can update it.
         predic_socket( 'aux', 'ampli on');
     }
     elseif ( $command == "amplioff" ) {
@@ -113,17 +122,17 @@
     }
 
     // USER MACROS are handled by the 'aux' server
+    elseif ( substr( $command, 0, 6 ) === "macro_" ) {
+        echo predic_socket( 'aux', $command );
+    }
     elseif ( $command === "list_macros" ) {
         $macros_array = scandir("/home/predic/macros/");
         echo json_encode( $macros_array );
     }
-    elseif ( substr( $command, 0, 6 ) === "macro_" ) {
-        echo predic_socket( 'aux', $command );
-    }
 
-    // PLAYER related commands are handled by the 'players' server
+    // PLAYERS related commands are handled by the 'players' server
     elseif ( substr( $command, 0, 7 ) === "player_" ) {
-        // A regular playback control (player_play, player_pause, etc)
+        // The expected playback control syntax is: 'player_play', 'player_pause', etc
         echo predic_socket( 'players', $command );
     }
     elseif ( substr( $command, 0, 4 ) === "http" ) {
@@ -131,7 +140,7 @@
         echo predic_socket( 'players', $command );
     }
 
-    //// Any else will be an STANDARD pre.di.c command, then handled by the 'control' server
+    // Any else will be an STANDARD pre.di.c CONTROL command, handled by the 'control' server
     else {
         echo predic_socket( 'control', $command );
     }
