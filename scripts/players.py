7a9e82aa643344261853cee1c6e118f0664d2e1e
Rsantct <rsantct@gmail.com>
2018-12-27 23:33:16 +0100

    Update players.py

--- scripts/players.py
+++ scripts/players.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 """
-    Starts / stops a server which listen for some player related tasks
-    supported by the 'players' module
+    Starts / stops a server that process trough by the 'players' module
     
     use:   scripts/players.py    start | stop
 """
 
-import sys
+import sys, os
 from subprocess import Popen
+from getconfigs import config
+
+server_path = os.path.expanduser( config['server_path'] )
 
 def start():
-    Popen( ['server_misc.py', 'players'] ) # if this fails check your paths
+    Popen( f'{server_path} players'.split() )
 
 def stop():
-    Popen( ['pkill', '-KILL', '-f',  'server_misc.py players'] )
+    Popen( [ 'pkill', '-KILL', '-f', f'{server_path} players' ] )
     # harakiri
-    Popen( ['pkill', '-KILL', '-f',  'scripts/players.py'] )
+    Popen( ['pkill', '-KILL', '-f',  'scripts/aux.py'] )
 
 if sys.argv[1:]:
     try:
