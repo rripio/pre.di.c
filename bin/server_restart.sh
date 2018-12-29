b22d4c940b33b090b334a587af188eb5d64ed7bb
Rsantct <rsantct@gmail.com>
2018-12-29 18:32:46 +0100

    Create server_restart.sh

---
+++ bin/server_restart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# kill the control server
+pkill -f "server.py control"
+
+# OjO hay veces que se queda algún proceso escuchando en 9999/tcp,
+# por ejemplo alsa_in ¿!?, hay que matarlo para liberar el puerto:
+# (i) Sometimes has been seen that some process can keep listening on 9999/tcp,
+#     for instance alsa_in ¿!?. Lets kill them in order to release our port:
+echo "releasing tcp/9999 ... .. ."
+fuser -kv 9999/tcp
+
+# Restarting the control server
+~/pre.di.c/bin/server.py control &
