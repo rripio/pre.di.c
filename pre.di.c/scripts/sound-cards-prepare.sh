ba2f0f40bafeeb17b32441d4b36b80bdb49a2f27
Rsantct <rsantct@gmail.com>
2019-01-04 00:14:08 +0100

    Update sound-cards-prepare.sh

--- pre.di.c/scripts/sound-cards-prepare.sh
+++ pre.di.c/scripts/sound-cards-prepare.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Useful script for Desktop and Pulseaudio systems
+# Useful script for Desktop and Pulseaudio systems.
+# If used, PLEASE place as FIRST position inside config/scripts
 
-# Release cards from Pulseaudio to be used in pre.di.c:
+# CARDS BELOW ARE A USER CASE, PLEASE UPDATE WITH YOURS
+
+# Release cards from Pulseaudio to be used by pre.di.c:
 # DX
 pactl set-card-profile alsa_card.pci-0000_02_04.0               off
 # miniStreamer
@@ -10,7 +13,8 @@ pactl set-card-profile alsa_card.usb-miniDSP_miniStreamer-01    off
 # Device
 pactl set-card-profile alsa_card.usb-0d8c_USB_Sound_Device-00   off
 
-# And restore our asound settigs for the cards used in pre.di.c:
+# And restore our asound settigs for the cards used by pre.di.c
+# (i) asound.XXX files can be stored after you have set properly adjustements on alsamixer
 alsactl -f ~/pre.di.c/config/asound.DX              restore DX
 alsactl -f ~/pre.di.c/config/asound.Device          restore Device
 alsactl -f ~/pre.di.c/config/asound.miniStreamer    restore miniStreamer
