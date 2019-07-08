a20a563f340cd5bd6aa955e16b3c44fb64668b3c
rripio <filarete@gmail.com>
2019-07-08 09:27:06 +0200

    use multiprocessing instead of threading

--- clients/mpd/mpd_load.py
+++ clients/mpd/mpd_load.py
@@ -28,9 +28,9 @@ use it with 'start' and 'stop' as options"""
 import os
 import sys
 import time
-import threading
 import math as m
 import subprocess as sp
+import multiprocessing as mp
 
 import mpd
 
@@ -55,40 +55,43 @@ def connect_mpd(mpd_host='localhost', mpd_port=6600, mpd_passwd=None):
 def mpd_vol_loop():
     """loop: reads mpd volume, sets predic volume"""
 
+    mpd_client = connect_mpd()
+    mpd_gain_min = gc.config['gain_max'] - mpd_conf['slider_range']
     while True:
-        interval = gc.config['command_delay'] / 10
-        mpd_client = connect_mpd()
+        mpd_client.idle('mixer')
         mpd_vol = int(mpd_client.status()['volume'])
+        # update pre.di.c level
+        predic_level = round((mpd_vol / 100
+                            * mpd_conf['slider_range'])
+                            + mpd_gain_min
+                            - gc.speaker['ref_level_gain'])
+        pd.client_socket("level " + str(predic_level), quiet=True)
+    mpd_client.close()
+    mpd_client.disconnect()
+
+
+def predic_vol_loop():
+    """loop: reads predic volume, sets mpd volume"""
+
+    interval = gc.config['command_delay'] / 10
+    predic_level = pd.get_state()['level']
+    mpd_gain_min = gc.config['gain_max'] - mpd_conf['slider_range']
+    while True:
+        # check level changes in pre.di.c
+        predic_level_old = predic_level
         predic_level = pd.get_state()['level']
-        mpd_gain_min = gc.config['gain_max'] - mpd_conf['slider_range']
-        try:
-            while True:
-                # check level changes in pre.di.c
-                predic_level_old = predic_level
-                predic_level = pd.get_state()['level']
-                if predic_level != predic_level_old:
-                    # update mpd "fake volume"
-                    predic_gain = predic_level + gc.speaker['ref_level_gain']
-                    mpd_vol = round((predic_gain - mpd_gain_min)
-                                            * 100 / mpd_conf['slider_range'])
-                    # minimal mpd volume
-                    if mpd_vol < 0: mpd_vol = 0
-                    mpd_client.setvol(int(mpd_vol))
-                # check volume changes in mpd
-                mpd_vol_old = mpd_vol
-                mpd_vol = int(mpd_client.status()['volume'])
-                if mpd_vol != mpd_vol_old:
-                    # update pre.di.c level
-                    predic_level = round((mpd_vol / 100
-                                        * mpd_conf['slider_range'])
-                                        + mpd_gain_min
-                                        - gc.speaker['ref_level_gain'])
-                    pd.client_socket("level " + str(predic_level), quiet=True)
-                time.sleep(interval)
-        except:
-            pass
-        mpd_client.close()
-        mpd_client.disconnect()
+        if predic_level != predic_level_old:
+            # update mpd "fake volume"
+            predic_gain = predic_level + gc.speaker['ref_level_gain']
+            mpd_vol = round((predic_gain - mpd_gain_min)
+                                    * 100 / mpd_conf['slider_range'])
+            # minimal mpd volume
+            if mpd_vol < 0:
+                mpd_vol = 0
+            mpd_client = connect_mpd()
+            mpd_client.setvol(int(mpd_vol))
+            mpd_client.close()
+        time.sleep(interval)
 
 
 def start():
@@ -96,7 +99,7 @@ def start():
 
     # create jack loop for connections
     # The jack_loop function will keep the loop alive, so we need to thread it
-    jloop = threading.Thread( target = pd.jack_loop, args=('mpd_loop',) )
+    jloop = mp.Process(target = pd.jack_loop, args=('mpd_loop',) )
     jloop.start()
 
     # starts MPD
@@ -116,10 +119,15 @@ def start():
                                                                  'OK MPD'):
             print('(mpd_load.py) mpd started :-)')
             try:
-                mpdloop = threading.Thread( target = mpd_vol_loop )
+                mpdloop = mp.Process( target = mpd_vol_loop )
                 mpdloop.start()
             except:
                 print('(mpd_load.py) mpd socket loop broke')
+            try:
+                predicloop = mp.Process( target = predic_vol_loop )
+                predicloop.start()
+            except:
+                print('(mpd_load.py) predic socket loop broke')
         else:
             print('(mpd_load.py) client_mpd didn\'t start')
 
