a9710a01aa7e16c43e772c73626bafa5c060b5d4
rripio <filarete@gmail.com>
2020-04-18 09:25:02 +0200

    example client of loop creation

---
+++ clients/stream_loop.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""sets up loop ports in jack """
+
+import sys
+import subprocess as sp
+import multiprocessing as mp
+
+import predic as pd
+
+def start():
+
+    # create jack loop for connections
+    # The jack_loop module will keep the loop alive, so we need to thread it.
+    #jd.jack_loop('alsa_loop')
+    jloop = mp.Process( target = pd.jack_loop, args=('stream_loop',) )
+    jloop.start()
+
+
+def stop():
+
+    sp.Popen('pkill -f stream_loop.py'.split())
+
+
+if sys.argv[1:]:
+    try:
+        option = {
+            'start' : start,
+            'stop'  : stop
+            }[sys.argv[1]]()
+    except KeyError:
+        print('stream_loop.py: bad option')
+else:
+    print(__doc__)
+
+
