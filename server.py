a1bb77d82979a1ebe7030b1b470575ae988e70d0
rripio <filarete@gmail.com>
2023-11-17 22:21:33 +0100

    revert d31bfeb reverts malfunction if closing launch terminal

--- server.py
+++ server.py
@@ -72,11 +72,13 @@ async def handle_commands(reader, writer):
         else:
             # command received in 'data', \
             # then send command to control.py
-            if init.config['verbose'] in [1, 2]:
-                print(f'\n(server) command: {data}')
 
             control.proccess_commands(data)
 
+            # this has to be after actual command sending
+            # if not, command execution fails when changing terminal
+            if init.config['verbose'] in [1, 2]:
+                print(f'\n(server) command: {data}')
             # a try block avoids blocking of state file writing \
             # when the terminal that launched startaudio.py is closed
             try:
